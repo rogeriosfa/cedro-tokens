dc22054b981eb4f386fc02167d02728390298452
Rogério Rodrigues <rogeriosfa@gmail.com>
2022-06-20 15:21:48 -0300

    Melhorando pacotes

---
+++ ios/fast/light/tokens.h
@@ -0,0 +1,25 @@
+
+//
+// tokens.h
+//
+
+// Do not edit directly
+// Generated on Mon, 20 Jun 2022 12:21:13 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define ButtonBorderRadius 4px
+#define ButtonTextSize 16.00f
+#define ButtonHeight 50px
+#define ColorFontPrimary [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f]
+#define ColorFontSecondary [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f]
+#define FontFamilyHeaders @"Montserrat"
+#define FontFamilyBase @"San Francisco"
+#define FontPlatformSystem @"San Francisco"
+#define SizeFontSmall 12.00f
+#define SizeFontMedium 16.00f
+#define SizeFontLarge 32.00f
+#define SizeFontBase 16.00f
+
